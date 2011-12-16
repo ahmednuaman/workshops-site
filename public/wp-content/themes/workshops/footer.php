7bdd42e0763a6a0192cf3830671c5c7a33ceafcf
Ahmed Nuaman <ahmed@ahmednuaman.com>
2011-12-16 20:00:09 +0000

    sorted scripts in the footer

--- public/wp-content/themes/workshops/footer.php
+++ public/wp-content/themes/workshops/footer.php
@@ -1,10 +1,23 @@
 			</div>
 		</div>
 		<?php if ( $_SERVER[ 'HTTP_HOST' ] === 'workshops.dev' ): ?>
-			<script src="/assets/js/jquery.js"></script>
-			<script src="/assets/js/suitcase.js"></script>
+			<script>
+			<!--
+				var s	= [
+					'<?php bloginfo( 'stylesheet_directory' ); ?>/assets/js/jquery.js',
+					'<?php bloginfo( 'stylesheet_directory' ); ?>/assets/js/suitcase.js',
+					'http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js'
+				];
+				var t	= ( new Date() ).getTime();
+				
+				while ( s.length )
+				{
+					document.write( '<script src="' + s.shift() + '?x=' + t + '"></' + 'script>' );
+				}
+			-->
+			</script>
 		<?php else: ?>
-			<script src="/assets/js/packaged.js"></script>
+			<script src="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/js/packaged.js"></script>
 		<?php endif; ?>
 	</body>
 </html>
