de5c7713bcea4dc8058865eae59e781af14a87cc
Ahmed Nuaman <ahmed@ahmednuaman.com>
2011-12-17 19:50:45 +0000

    set up front page

--- public/wp-content/themes/workshops/front-page.php
+++ public/wp-content/themes/workshops/front-page.php
@@ -1,3 +1,20 @@
 <?php get_header(); ?>
-
+<div id="learning">
+	
+</div>
+<div id="who">
+	
+</div>
+<div id="dates">
+	
+</div>
+<div id="cost">
+	
+</div>
+<div id="signup">
+	
+</div>
+<div id="footnote">
+	
+</div>
 <?php get_footer(); ?>
