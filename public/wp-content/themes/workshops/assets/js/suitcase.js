2334db17d163131e179411d39e33c04f06e61bc7
Ahmed Nuaman <ahmed@ahmednuaman.com>
2011-12-17 19:50:28 +0000

    added easing and scrolling

--- public/wp-content/themes/workshops/assets/js/suitcase.js
+++ public/wp-content/themes/workshops/assets/js/suitcase.js
@@ -1,7 +1,26 @@
 var S	= {
+	cssAnimation												: 'webkitAnimationEnd animationend oAnimationEnd',
+	cssTransition												: 'webkitTransitionEnd transitionend oTransitionEnd',
+	
 	ready														: function()
 	{
+		var h	= window.location.hash;
+		
 		S.detectBrowser();
+		
+		if ( h.length > 0 )
+		{
+			S.scrollTo( h );
+		}
+	},
+	
+	scrollTo													: function(h)
+	{
+		var y	= $( h ).offset().top - $( '#menubar' ).outerHeight();
+		
+		$( 'html, body' ).animate({
+			'scrollTop'	: y
+		}, 2000, 'easeInOutExpo' );
 	},
 	
 	detectBrowser												: function()
@@ -61,6 +80,16 @@ var S	= {
 		$( 'html' ).removeClass( 'no-js' );
 	}
 };
+
+$.extend($.easing,
+{
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	}
+});
 
 // var _gaq=[['_setAccount','UA-XXX'],['_trackPageview'],['_trackPageLoadTime']];
 // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
